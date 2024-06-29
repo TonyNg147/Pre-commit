52a95c48702a811e392b6e176841e8d711d91cb7
Tony <TonyNg>
2024-06-29 14:53:55 +0700

    Update

---
+++ src/source.h
@@ -0,0 +1,13 @@
+#ifndef SOURCE_H
+#define SOURCE_H
+
+class Source
+{
+public:
+    Source();
+private:
+    int m_first=0;
+    int m_first2=0;    int m_first3=0;int m_first4=0;
+};
+
+#endif // SOURCE_H
